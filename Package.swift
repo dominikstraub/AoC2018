4416bc797cb1b7659a5a2fa9a88af6f8ed9a19cc
Dominik Straub <ds@kadenpartner.ch>
2020-11-27 18:38:33 +0100

    Day1

--- Package.swift
+++ Package.swift
@@ -13,8 +13,13 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Day1",
+            name: "Utils",
             dependencies: []),
+        .target(
+            name: "Day1",
+            dependencies: ["Utils"],
+            resources: [.process("input.txt"), .process("test.txt")]
+        ),
         .testTarget(
             name: "Day1Tests",
             dependencies: ["Day1"]),
--- Sources/Day1/main.swift
+++ Sources/Day1/main.swift
@@ -1,1 +1,35 @@
-print("Hello Day1!")
+import Foundation
+import Utils
+
+// let input = try Utils.getInput(day: 1, file: "test.txt")
+let input = try Utils.getInput(day: 1)
+
+let items = input
+.components(separatedBy: CharacterSet(charactersIn: ", \n"))
+.map { (item) -> Int? in
+    return Int(item)
+}
+.compactMap { $0 }
+
+func part1(items: [Int]) -> Int {
+    return items.reduce(0) { $0 + $1 }
+}
+
+print("Part 1: \(part1(items: items))")
+
+func part2(items: [Int]) -> Int {
+    var shift = 0
+    var shifts: [Int: Bool] = [:]
+    shifts[shift] = true
+    while true {
+        for change in items {
+            shift += change
+            if shifts[shift] ?? false {
+                return shift
+            }
+            shifts[shift] = true
+        }
+    }
+}
+
+print("Part 2: \(part2(items: items))")
